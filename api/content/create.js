06dad79aab65ef2a10ce9e0cdbfea90b3b4af3de
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-15 16:59:52 +0300

    Refactor content.create function

--- api/content/create.js
+++ api/content/create.js
@@ -6,31 +6,8 @@ const dynamoDb = require('../dynamodb');
 
 module.exports.create = (event, context, callback) => {
     const data = JSON.parse(event.body);
-
     validateContent(data);
-
-    const params = {
-        TableName: process.env.CONTENT_TABLE,
-        Item: {
-            id: uuid.v1(),
-            short_name: data.short_name,
-            url: data.url
-        },
-    };
-
-    dynamoDb.put(params, (error) => {
-        if (error) {
-            console.error(error);
-            callback(new Error('Couldn\'t create the content.'));
-            return;
-        }
-
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(params.Item),
-        };
-        callback(null, response);
-    });
+    createContent(data, callback);
 };
 
 function validateContent(content) {
@@ -40,5 +17,40 @@ function validateContent(content) {
 
     if (typeof content.short_name !== 'string' || typeof content.url !== 'string') {
         throw new Error('short_name and url fields should be a string');
+    }
+}
+
+function createContent(content, callback) {
+    let params = initParamsForCreation(content);
+    performPut(params, callback);
+}
+
+function initParamsForCreation(content) {
+    return {
+        TableName: process.env.CONTENT_TABLE,
+        Item: {
+            id: uuid.v1(),
+            short_name: content.short_name,
+            url: content.url
+        }
+    }
+}
+
+function performPut(params, callback) {
+    dynamoDb.put(params, (error) => {
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t create the content.'));
+            return;
+        }
+
+        callback(null, createResponse(params));
+    });
+}
+
+function createResponse(params) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(params.Item)
     }
 }
