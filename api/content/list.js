288dcfd0fb49d889d5ebb5e6816c8d2fa64339b0
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-15 19:17:27 +0300

    Use response helper in content.list

--- api/content/list.js
+++ api/content/list.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const dynamoDb = require('../dynamodb');
+const responseHelper = require('../helpers/http_response_helper');
+
 const params = {
     TableName: process.env.CONTENT_TABLE,
 };
@@ -8,18 +10,10 @@ const params = {
 module.exports.list = (event, context, callback) => {
     dynamoDb.scan(params, (error, result) => {
         if (error) {
-            console.error(error);
-            callback(new Error('Couldn\'t fetch the todos.'));
+            callback(null, responseHelper.createResponseWithError(500, 'Couldn\'t fetch the todos.'));
             return;
         }
 
-        callback(null, createResponse(result));
+        callback(null, responseHelper.createSuccessfulResponse(result.Items));
     });
 };
-
-function createResponse(params) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(params.Items)
-    }
-}
