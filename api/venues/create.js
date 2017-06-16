5ded4cf9027c6261a796d57593ce7f60cc5c7a7f
Artem Klots <artemkl@softwareplanetgroup.com>
2017-06-16 09:39:37 +0300

    merge files structure

--- api/venues/create.js
+++ api/venues/create.js
@@ -1,5 +1,5 @@
 'use strict';
-let Venue = require('./../entities/venue');
+let Venue = require('././venue');
 
 const dynamodb = require('./../dynamodb');
 const tableName = process.env.VENUES_TABLE;
